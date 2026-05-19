f901eb86178a7a91a77aa5ffdf7f1a2898cbf5b2
yungpotato <janina.valkonen@tuni.fi>
2026-05-19 10:47:49 +0300

    Move source files to src and update transcript

--- scr/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Resolves the absolute path of the project root directory
-# based on the location of this config file
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
